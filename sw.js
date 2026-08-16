ac5515a7047b31ae32fe8a956adb0c069afc6c93
askmobiles <info@askegoods.com>
2026-08-16 17:56:26 -0400

    update

--- sw.js
+++ sw.js
@@ -25,7 +25,7 @@
      * The runtime cache is capped, so a tablet never fills up with 250 pages
        and a 2.4 MB French dictionary it will not read again.
 */
-var VERSION = "0c7186b45460";
+var VERSION = "c70e8ba6f2c1";
 var SHELL = "cq-shell-" + VERSION;
 var RUNTIME = "cq-run-" + VERSION;
 var MAX_RUNTIME = 220;
@@ -40,7 +40,7 @@ var PRECACHE = [
   "driving-test.html",
   "daily.html",
   "css/style.css?v=a008388b",
-  "js/site.js?v=7cb7e820",
+  "js/site.js?v=01a3ace0",
   "js/game-fullscreen.js?v=c56db4e5",
   "js/endcard.js?v=e6598278",
   "js/tv-mode.js?v=4ebd9ddf",
